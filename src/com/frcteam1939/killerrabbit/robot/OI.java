979523e72862a3b69137a628e0cb953cb709717f
learnSola <sdugbo@learnscienceandmathclub.org>
2017-01-16 12:54:41 -0600

    Test shooter code

--- src/com/frcteam1939/killerrabbit/robot/OI.java
+++ src/com/frcteam1939/killerrabbit/robot/OI.java
@@ -1,10 +1,36 @@
 package com.frcteam1939.killerrabbit.robot;
 
+import com.frcteam1939.killerrabbit.robot.commands.shooter.PushFrisbee;
+import com.frcteam1939.killerrabbit.robot.commands.shooter.RetractFrisbee;
+import com.frcteam1939.killerrabbit.robot.commands.shooter.Shoot;
+import com.frcteam1939.killerrabbit.robot.commands.shooter.StartWheels;
+import com.frcteam1939.killerrabbit.robot.commands.shooter.StopWheels;
+
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
 
 public class OI {
 
 	public final Joystick left = new Joystick(0);
 	public final Joystick right = new Joystick(1);
 
+	{
+		JoystickButton shoot = new JoystickButton(left, 0);
+		shoot.whenPressed(new Shoot());
+		
+		JoystickButton push = new JoystickButton(left, 3);
+		push.whenPressed(new PushFrisbee());
+		
+		JoystickButton retract = new JoystickButton(left, 2);
+		retract.whenPressed(new RetractFrisbee());
+		
+		JoystickButton startWheels = new JoystickButton(left, 4);
+		startWheels.whenPressed(new StartWheels());
+		
+		JoystickButton stopWheels = new JoystickButton(left, 5);
+		stopWheels.whenPressed(new StopWheels());
+		
+
+	}
+	
 }
--- src/com/frcteam1939/killerrabbit/robot/Robot.java
+++ src/com/frcteam1939/killerrabbit/robot/Robot.java
@@ -1,6 +1,8 @@
 
 package com.frcteam1939.killerrabbit.robot;
 
+import com.frcteam1939.killerrabbit.robot.subsystems.Shooter;
+
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
@@ -13,7 +15,7 @@ import edu.wpi.first.wpilibj.livewindow.LiveWindow;
  * directory.
  */
 public class Robot extends IterativeRobot {
-
+	public static final Shooter shooter= new Shooter();
 	public static OI oi;
 
 	/**
