7d45c1339d166b004cafc4bacdc971c200961f2c
Joey Holliday <joeyholliday77@gmail.com>
2017-01-16 10:43:20 -0600

    Upgrade for 2017 plugins and new control system

---
+++ src/com/frcteam1939/killerrabbit/robot/OI.java
@@ -0,0 +1,10 @@
+package com.frcteam1939.killerrabbit.robot;
+
+import edu.wpi.first.wpilibj.Joystick;
+
+public class OI {
+
+	public final Joystick left = new Joystick(0);
+	public final Joystick right = new Joystick(1);
+
+}
