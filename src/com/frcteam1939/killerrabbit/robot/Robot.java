4973941004b69cf7b2764488b7cecc57e6cbe9da
azhawa <azhawa@hotmail.com>
2017-01-16 13:00:02 -0600

    Edits

--- src/com/frcteam1939/killerrabbit/robot/Robot.java
+++ src/com/frcteam1939/killerrabbit/robot/Robot.java
@@ -12,20 +12,13 @@ public class Robot extends IterativeRobot {
 	public static final Drivetrain drivetrain = new Drivetrain();
 	public static OI oi;
 
-	/**
-	 * This function is run when the robot is first started up and should be
-	 * used for any initialization code.
-	 */
+	
 	@Override
 	public void robotInit() {
 		oi = new OI();
 	}
 
-	/**
-	 * This function is called once each time the robot enters Disabled mode.
-	 * You can use it to reset any subsystem information you want to clear when
-	 * the robot is disabled.
-	 */
+	
 	@Override
 	public void disabledInit() {
 
@@ -36,25 +29,13 @@ public class Robot extends IterativeRobot {
 		Scheduler.getInstance().run();
 	}
 
-	/**
-	 * This autonomous (along with the chooser code above) shows how to select
-	 * between different autonomous modes using the dashboard. The sendable
-	 * chooser code works with the Java SmartDashboard. If you prefer the
-	 * LabVIEW Dashboard, remove all of the chooser code and uncomment the
-	 * getString code to get the auto name from the text box below the Gyro
-	 *
-	 * You can add additional auto modes by adding additional commands to the
-	 * chooser code above (like the commented example) or additional comparisons
-	 * to the switch structure below with additional strings & commands.
-	 */
+	
 	@Override
 	public void autonomousInit() {
 
 	}
 
-	/**
-	 * This function is called periodically during autonomous
-	 */
+	
 	@Override
 	public void autonomousPeriodic() {
 		Scheduler.getInstance().run();
@@ -65,17 +46,13 @@ public class Robot extends IterativeRobot {
 
 	}
 
-	/**
-	 * This function is called periodically during operator control
-	 */
+
 	@Override
 	public void teleopPeriodic() {
 		Scheduler.getInstance().run();
 	}
 
-	/**
-	 * This function is called periodically during test mode
-	 */
+
 	@Override
 	public void testPeriodic() {
 		LiveWindow.run();
--- src/com/frcteam1939/killerrabbit/robot/commands/DriveByJoystick.java
+++ src/com/frcteam1939/killerrabbit/robot/commands/DriveByJoystick.java
@@ -11,26 +11,20 @@ public class DriveByJoystick extends Command {
     	requires(Robot.drivetrain);
     }
 
-    // Called just before this Command runs the first time
     protected void initialize() {
     }
 
-    // Called repeatedly when this Command is scheduled to run
     protected void execute() {
     	Robot.drivetrain.drive(Robot.oi.left.getX(), Robot.oi.right.getY(), Robot.oi.right.getX());
     }
 
-    // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
         return false;
     }
 
-    // Called once after isFinished returns true
     protected void end() {
     }
 
-    // Called when another command which requires one or more of the same
-    // subsystems is scheduled to run
     protected void interrupted() {
     }
 }
