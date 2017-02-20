fb484f5d7920b0685f323a898cd4a566af64a372
PCApple <poc.kcchiefs@gmail.com>
2017-02-20 11:06:01 -0600

    Revert "Vision is working, pleasecheck this out joey"
    This reverts commit b0c8fc0aeab0f35bb8298a0f9d3db2b1ee2a2d18.

--- src/com/frcteam1939/killerrabbit/robot/Robot.java
+++ src/com/frcteam1939/killerrabbit/robot/Robot.java
@@ -40,14 +40,13 @@ public class Robot extends IterativeRobot {
 		new Thread(() -> {
             UsbCamera camera = CameraServer.getInstance().startAutomaticCapture();
             camera.setResolution(640, 480);
-            camera.setBrightness(10);
             CvSink cvSink = CameraServer.getInstance().getVideo();
             CvSource outputStream = CameraServer.getInstance().putVideo("Processed", 640, 480);
             Mat source = new Mat();
             while(!Thread.interrupted()) {
                 cvSink.grabFrame(source);
                 pipe.process(source);
-                outputStream.putFrame(pipe.rgbThresholdOutput());
+                outputStream.putFrame(pipe.hsvThresholdOutput());
             }
         }).start();
 	    
